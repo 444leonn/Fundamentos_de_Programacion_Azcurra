8ee36a9ba61d0178088511a3e074057d38cf94f9
Leon <acostaleon25@gmail.com>
2025-05-24 19:21:09 -0300

    Agrego consigna ejercicio 6

---
+++ Structs/Guia_Structs/6_structs.c
@@ -0,0 +1,14 @@
+/*
+* Una empresa de transporte de pasajeros de larga distancia posee micros de 4 categorías y viaja a 250 localidades de zonas turísticas del país.
+* No necesariamente todas las temporadas se habilitan las 4 categorías, ni hay viajes a todas las localidades posibles.
+* Se dispone de una tabla con la información de todos los pasajes vendidos en una temporada consistente en:
+*     código de categoría (a, b, c, d)
+*     código de destino (1 a 250)
+* También se dispone de un listado ordenado por código de destino con el nombre de cada localidad.
+* Se pide informar :
+*     a) La cantidad de pasajeros por localidad, por categoría.
+*     b) La cantidad de pasajeros por localidad.
+*     c) La cantidad de pasajeros por categoría.
+*     d) El nombre de la localidad a la que viajó la mayor cantidad de pasajeros.
+*     e) El nombre de la localidad a la que viajó la menor cantidad de pasajeros.
+*/
