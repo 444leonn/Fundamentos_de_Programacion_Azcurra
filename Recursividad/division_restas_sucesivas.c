92ac30327531f1852ecac26d216ebf5c3819a31c
leon <acostaleon25@gmail.com>
2025-06-18 18:41:27 -0300

    Se agrega la actividad obligatoria

---
+++ Recursividad/division_restas_sucesivas.c
@@ -0,0 +1,15 @@
+// Implementar una funcion que permita dividir dos numeros enteros por restas sucesivas.
+
+#include <stdio.h>
+
+int division_sucesiva(num) {
+    if (numero == 0) {
+        
+    }
+}
+
+int main() {
+    int num;
+
+    division_sucesiva(num);
+}
