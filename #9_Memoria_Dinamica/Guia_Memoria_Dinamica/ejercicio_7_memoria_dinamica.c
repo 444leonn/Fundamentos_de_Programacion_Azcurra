e19cbb40b465c01b0d39551a1ce4c8be6ddcb46d
leon <acostaleon25@gmail.com>
2025-07-07 20:51:12 -0300

    agregado ejercicio 7

--- #9_Memoria_Dinamica/Guia_Memoria_Dinamica/ejercicio_7_memoria_dinamica.c
+++ #9_Memoria_Dinamica/Guia_Memoria_Dinamica/ejercicio_7_memoria_dinamica.c
@@ -3,3 +3,23 @@
 * el cual apunta a un bloque de memoria dinámica, respetando la siguiente declaración.
 * En caso de no poder reservar la memoria, retornar NULL.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STR 30
+typedef struct {
+    int padron;
+    char nombre[MAX_STR];
+    char apellido[MAX_STR];
+} t_alumno;
+
+t_alumno *crear_alumnos(int cantidad) {
+    return malloc(cantidad * sizeof(t_alumno));
+}
+
+int main() {
+    t_alumno *crear_alumnos(int n);
+    
+    return 0;
+}
